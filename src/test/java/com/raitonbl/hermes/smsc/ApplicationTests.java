c90118cde443fdc098dcad45c1774908a418cca4
Delfim Marime <delfimarime@gmail.com>
2024-02-17 12:39:28 +0200

    wip: project setup

---
+++ src/test/java/com/raitonbl/hermes/smsc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raitonbl.hermes.smsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
