fc7702af26f874a293a8f23e47c2dae8dc887d4b
Delfim Marime <delfimarime@gmail.com>
2024-03-02 16:24:18 +0200

    feat: GET/PUT rules added

--- src/main/java/com/raitonbl/hermes/smsc/camel/http/GetRuleEndpointRouteBuilder.java
+++ src/main/java/com/raitonbl/hermes/smsc/camel/http/GetRuleEndpointRouteBuilder.java
@@ -8,12 +8,16 @@ import com.raitonbl.hermes.smsc.config.RuleConfiguration;
 import com.raitonbl.hermes.smsc.model.Problem;
 import jakarta.inject.Inject;
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.Builder;
+import org.apache.camel.builder.PredicateBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.dataformat.YAMLDataFormat;
 import org.apache.camel.model.dataformat.YAMLLibrary;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 
 @Component
 public class GetRuleEndpointRouteBuilder extends AdvancedRouteBuilder {
@@ -29,10 +33,21 @@ public class GetRuleEndpointRouteBuilder extends AdvancedRouteBuilder {
         from("rest:GET:/rules")
                 .routeId(GET_RULES_ENDPOINT_ROUTE_ID)
                 .doTry()
+                    .choice()
+                        .log(LoggingLevel.DEBUG, "GET /rules has Content-Type=${headers."+Exchange.CONTENT_TYPE+"}")
+                        .when(PredicateBuilder.not( header(Exchange.CONTENT_TYPE).in(MediaType.APPLICATION_JSON_VALUE,MediaType.TEXT_PLAIN_VALUE) ))
+                            .throwException(new HttpMediaTypeNotSupportedException("MediaType doesn't match"+MediaType.TEXT_PLAIN_VALUE+" nor "+MediaType.APPLICATION_JSON_VALUE))
+                    .end()
                     .to(RuleRouteBuilder.DIRECT_TO_READ_RULES_ROUTE_ID)
                     .removeHeaders("*")
                     .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.OK.value()))
                     .setHeader(Exchange.CONTENT_TYPE, simple(MediaType.APPLICATION_JSON_VALUE))
+                .endDoTry()
+                .doCatch(HttpMediaTypeNotSupportedException.class)
+                    .log("Exception caught ${exception.stacktrace}")
+                    .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+                    .setHeader(Exchange.CONTENT_TYPE, simple(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                    .setBody(Problem.unsupportedMediaType(ENDPOINT_OPERATION_ID))
                 .doCatch(Exception.class)
                     .log("Exception caught ${exception.stacktrace}")
                     .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(HttpStatus.INTERNAL_SERVER_ERROR.value()))
