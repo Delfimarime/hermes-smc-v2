421360e7fd1fd957bc049824e65939cc06b4d44e
Delfim Marime <delfimarime@gmail.com>
2024-03-02 16:25:50 +0200

    feat: GET/PUT rules added

--- src/main/java/com/raitonbl/hermes/smsc/camel/HermesConstants.java
+++ src/main/java/com/raitonbl/hermes/smsc/camel/HermesConstants.java
@@ -3,7 +3,5 @@ package com.raitonbl.hermes.smsc.camel;
 public final class HermesConstants {
    public static final String SEND_REQUEST_ID = "CamelHermesSmppId";
    public static final String MESSAGE_RECEIVED_BY = "CamelHermesSmppFrom";
-   public static final String RULES_CACHE_KEY = "CamelHermesSmppDeciderRule";
-
 
 }
